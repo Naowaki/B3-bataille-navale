283da9fa2e7e9570339ff5970d74638996c41ca1
Thom <Thom@192.168.8.76>
2016-06-03 10:52:36 +0200

    Ajout premiers attributs et constructeur

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,4 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Plateau {
-
+	private Map<String, Integer> cases;
+	private Map<Map, Boolean> cibles;
+	
+	public Plateau() {
+		this.cases = new HashMap<String, Integer>();
+		this.cibles = new HashMap<Map, Boolean>();
+	}
 }
