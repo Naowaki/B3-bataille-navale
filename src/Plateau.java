bb223b98261b759aa2876eda38f71ffd81b66f2f
Thom <Thom@192.168.8.76>
2016-06-03 11:16:04 +0200

    Création liste des cases

--- src/Plateau.java
+++ src/Plateau.java
@@ -3,10 +3,18 @@ import java.util.Map;
 
 public class Plateau {
 	private Map<String, Integer> cases;
-	private Map<Map, Boolean> cibles;
+	private Map<Map<String, Integer>, Boolean> cibles;
 	
 	public Plateau() {
 		this.cases = new HashMap<String, Integer>();
-		this.cibles = new HashMap<Map, Boolean>();
+		this.cibles = new HashMap<Map<String, Integer>, Boolean>();
+		
+		String[] lettres = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+		
+		for (int i = 1; i <= 10; i++) {
+			for (String a: lettres) {
+				this.cases.put(a, i);
+			}
+		}
 	}
 }
