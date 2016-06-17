a7501498913474aa9286a7c0ef443bb3e58239b1
Courbin Francis <francis.courbin@epsi.fr>
2016-06-17 17:35:57 +0200

    modif main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-
 public class Main {
 
+	public static void main(){
+		Plateau platoJ1 = new Plateau();
+		Plateau platoJ2 = new Plateau();
+		Player Joueur1 = new Player("Jean-Charle", platoJ1);
+		Player Joueur2 = new Player("In the Navy", platoJ2);
+	}
+	
 }
